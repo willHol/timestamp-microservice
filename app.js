64d46ea608aeb3c26860a39c79b877c881ceaa0a
William <thenecroyeti@gmail.com>
2017-02-27 01:49:39 +1300

    Added heroku integration

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/:date', function(request, response) {
 	// Send the object created by dateParser as JSON with JSONP support
-	var dateObj = dateParser(request.params.date);
+	let dateObj = dateParser(request.params.date);
 	response.jsonp(dateObj);
 });
 
