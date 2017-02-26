a156d003b64b848ad3a33a133bbcd448fe839538
William <thenecroyeti@gmail.com>
2017-02-27 01:03:54 +1300

    Updated app.js to use dateParser middleware

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dateParser = require('date-parser');
-const routes = require('./routes/index');
 
 const app = express();
 
-app.use(dateParser);
-app.use('/', routes);
+app.use('/', dateParser);
+
+app.get('/', function(request, response) {
+	// Send the object created by dateParser as JSON with JSONP support
+	response.jsonp(response.locals.transformedDate);
+});
 
 app.listen(3000 || process.env.PORT);
